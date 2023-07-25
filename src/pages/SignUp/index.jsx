dc70bda8d43581245b9a94c58322db929820944d
Bruna Porato <brunaporato@gmail.com>
2023-07-25 18:26:15 -0300

    add: user signup

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,12 +1,40 @@
 import { Container } from "./styles";
 
-import { Link } from "react-router-dom";
+import { api } from "../../services/api";
+
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import polygon from "../../assets/polygon.svg"
 import { InputDishInfo } from "../../components/InputDishInfo";
 import { Button } from "../../components/Button";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+     return alert("Preencha todos os campos");
+    }
+
+    api.post("/users", { name, email, password })
+    .then(() => {
+      alert("Usuário cadastrado com sucesso");
+      navigate("/");
+    })
+    .catch(error => {
+      if(error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível cadastrar")
+      }
+    });
+  }
+
   return (
     <Container>
       <div className="top">
@@ -16,17 +44,33 @@ export function SignUp() {
       <form action="">
         <div className="input-wrapper">
             <label htmlFor="name">Seu nome</label>
-            <InputDishInfo id="name" placeholder="Ex.: Maria da Silva" type="text"/>
+            <InputDishInfo
+              id="name"
+              placeholder="Ex.: Maria da Silva"
+              type="text"
+              onChange={e => setName(e.target.value)}
+            />
           </div>
           <div className="input-wrapper">
             <label htmlFor="email">Email</label>
-            <InputDishInfo id="email" placeholder="exemplo@exemplo.com.br" type="email"/>
+            <InputDishInfo
+              id="email"
+              placeholder="exemplo@exemplo.com.br"
+              type="email"
+              onChange={e => setEmail(e.target.value)}
+            />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Senha</label>
-            <InputDishInfo id="password" placeholder="Mínimo 6 caracteres" type="password" minLength={6}/>
+            <InputDishInfo
+              id="password"
+              placeholder="Mínimo 6 caracteres"
+              type="password"
+              minLength={6}
+              onChange={e => setPassword(e.target.value)}
+            />
           </div>
-          <Button type="submit" title="Criar conta" />
+          <Button type="button" title="Criar conta" onClick={handleSignUp} />
       </form>
       <Link to="/">Já tenho uma conta</Link>
     </Container>
