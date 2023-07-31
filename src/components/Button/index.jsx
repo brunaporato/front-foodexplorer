2f1b9162604d462622dc78e8bace88e52452cdf9
Bruna Porato <brunaporato@gmail.com>
2023-07-31 20:58:09 -0300

    add: create dish

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,6 @@ export function Button({ title, loading = false, ...rest }) {
   return (
     <Container
     type="button"
-    disabled={loading}
     {...rest}
     >
      { loading ? 'carregando...' : title }
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,6 @@
 import { Container } from "./styles";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { InputDishInfo } from "../../components/InputDishInfo";
 import { Header } from "../../components/Header";
@@ -10,10 +10,19 @@ import { NewIngredient } from "../../components/NewIngredient";
 
 import { FiChevronLeft, FiChevronDown, FiUpload } from "react-icons/fi";
 import { useState } from "react";
+import { api } from "../../services/api";
 
 export function New() {
   const [ingredients, setIngredients] = useState([]);
   const [newIngredient, setNewIngredient] = useState("");
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
+
+  const navigate = useNavigate();
+  let response;
 
   function handleAddIngredient() {
     setIngredients(prevState => [...prevState, newIngredient]);
@@ -22,6 +31,41 @@ export function New() {
 
   function handleRemoveIngredient(deleted) {
     setIngredients(prevState => prevState.filter(ingredient => ingredient != deleted));
+  }
+
+  async function handleImage(e) {
+    const file = e.target.files[0];
+    setImage(file);
+
+    const fileUploadForm = new FormData();
+    fileUploadForm.append("image", file);
+
+    response = await api.patch("/foodimage", fileUploadForm);
+  }
+
+  async function handleNewDish() {
+    // return console.log(image)
+
+    try {
+      await api.post("/foods", {
+        name,
+        category,
+        price,
+        description,
+        ingredients,
+        image: response.data.image
+      });
+
+      alert("Prato criado com sucesso");
+      navigate("/");
+    } catch(error) {
+      if(error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível cadastrar este prato")
+      }
+    }
+
   }
 
   return (
@@ -34,20 +78,34 @@ export function New() {
           <div className="input-wrapper">
             <label htmlFor="image">Imagem do Prato</label>
             <label className="inputImage" htmlFor="image"><FiUpload size={24} /> Selecione imagem</label>
-            <InputDishInfo id="image" type="file" placeholder="Selecione imagem" />
+            <InputDishInfo
+              id="image"
+              type="file"
+              placeholder="Selecione imagem"
+              onChange={handleImage}
+            />
           </div>
 
           <div className="input-wrapper">
             <label htmlFor="name">Nome</label>
-            <InputDishInfo id="name" type="text" placeholder="Ex.: Salada Ceasar" />
+            <InputDishInfo
+              id="name"
+              type="text"
+              placeholder="Ex.: Salada Ceasar"
+              onChange={e => setName(e.target.value)}
+            />
           </div>
 
           <div className="input-wrapper">
             <label htmlFor="category">Categoria</label>
             <div className="select">
-              <select id="category">
-                <option value="1">Refeição</option>
-                <option value="1">Refeição</option>
+              <select
+                id="category"
+                onChange={e => setCategory(e.target.value)}
+              >
+                <option value=""></option>
+                <option value="Refeição">Refeição</option>
+                <option value="Snack">Snack</option>
               </select>
               <label className="arrowDown" htmlFor="category"><FiChevronDown size={24} /></label>
             </div>
@@ -77,15 +135,27 @@ export function New() {
 
           <div className="input-wrapper">
             <label htmlFor="price">Preço</label>
-            <InputDishInfo id="price" type="number" placeholder="R$00,00" />
+            <InputDishInfo
+              id="price"
+              type="number"
+              placeholder="R$00,00"
+              onChange={e => setPrice(e.target.value)}
+            />
           </div>
 
           <div className="input-wrapper">
             <label htmlFor="description">Descrição</label>
-            <textarea id="description" placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"></textarea>
+            <textarea
+              id="description"
+              placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+              onChange={e => setDescription(e.target.value)}
+            />
           </div>
 
-          <Button title="Salvar alterações" disabled />
+          <Button
+            title="Salvar alterações"
+            onClick={handleNewDish}
+          />
         </form>
       </div>
       <Footer />
